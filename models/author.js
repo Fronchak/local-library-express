2c1ecb09767be4015463191f1668243350efd01b
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-08-27 17:04:37 -0300

    Criado as classes models

---
+++ models/author.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const AuthorSchema = new mongoose.Schema({
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date }
+});
+
+AuthorSchema
+    .virtual("name")
+    .get(function() {
+        let fullName = "";
+        if(this.first_name && this.family_name) {
+            fullName: `${this.family_name}, ${this.first_name}`
+        }
+        return fullName;
+    });
+
+AuthorSchema
+    .virtual("url")
+    .get(function() {
+        return `/catalog/author/${this._id}`;
+    });
+
+module.exports = mongoose.model("Author", AuthorSchema);
