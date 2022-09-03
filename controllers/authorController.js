b65cb5eeb6f50d28e1bab0b7592cbc50a9a96d91
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-09-03 10:57:08 -0300

    Implementado update dos autores, junto com a correção da formatação das datas'

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const { body, validationResult } = require("express-validator");
 const Author = require("../models/author");
 const Book = require("../models/book");
+const getDatePlusTimeZoneOffSet = require('../util/getDatePlusTimeZoneOffSet');
 
 exports.author_list = (req, res, next) => {
     Author
@@ -68,16 +69,23 @@ exports.author_create_post = [
         .isISO8601()
         .toDate(),
     (req, res, next) => {
-        console.log(req.body.date_of_birth);
+        const date_of_birth = req.body.date_of_birth && getDatePlusTimeZoneOffSet(req.body.date_of_birth);
+        const date_of_death = req.body.date_of_death && getDatePlusTimeZoneOffSet(req.body.date_of_death);
+        const author = new Author({
+          first_name: req.body.first_name,
+          family_name: req.body.family_name,
+          date_of_birth,
+          date_of_death,
+        });
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.render("authorForm", { 
+            res.render("authorForm", { 
                 title: "Create Author", 
-                author: req.body,
+                author,
                 errors: errors.array() 
             });
+            return;
         }
-        const author = new Author(req.body);
         author.save((err) => {
             if (err) return next(err);
             return res.redirect(author.url);
@@ -137,10 +145,64 @@ exports.author_delete_post = (req, res) => {
     });
 };
 
-exports.author_update_get = (req, res) => {
-    res.send("Not implemented: Author update get");
+exports.author_update_get = (req, res, next) => {
+    const id = mongoose.Types.ObjectId(req.params.id);
+    Author.findById(id)
+      .exec((err, author) => {
+        if (err) return next(err);
+        res.render('authorForm', {
+          title: 'Update Author',
+          author
+        });
+        return;
+      });
 }
 
-exports.author_update_post = (req, res) => {
-    res.send("Not implemented: Author update post");
-};
+exports.author_update_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('First name must be especified.')
+    .isAlpha()
+    .withMessage('First name shoud only have simple characteres.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Family name must be specified')
+    .isAlpha()
+    .withMessage('Family name shoud only have simple characteres.'),
+  body('date_of_birth', 'Invalid date for date of birth.')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death','Invalid date for date of death.')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const date_of_birth = req.body.date_of_birth && getDatePlusTimeZoneOffSet(req.body.date_of_birth);
+    const date_of_death = req.body.date_of_death && getDatePlusTimeZoneOffSet(req.body.date_of_death);
+    const id = mongoose.Types.ObjectId(req.params.id);
+    const author = new Author({
+      _id: id,
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth,
+      date_of_death,
+    });
+    if (!errors.isEmpty()) {
+      res.render('authorForm', {
+        title: 'Update Author',
+        author,
+        errors: errors.array(),
+      });
+      return;
+    }
+    Author.findByIdAndUpdate(id, author, {}, (err, updatedAuthor) => {
+      if (err) return next(err);
+      res.redirect(updatedAuthor.url);
+      return;
+    });
+  },
+];
--- models/author.js
+++ models/author.js
@@ -25,26 +25,23 @@ AuthorSchema
     });
 
 AuthorSchema
-    .virtual("birthDateFormatted")
+    .virtual('birthDateISOFormatted')
     .get(function() {
-        return (this.date_of_birth) ? 
-            this.date_of_birth.toLocaleDateString("en-US", { dateStyle: 'long' }) : "";
+      return this.date_of_birth && DateTime.fromJSDate(this.date_of_birth).toISODate();
     });
+
+AuthorSchema
+    .virtual('deathDateISOFormatted')
+    .get(function() {
+      return this.date_of_death && DateTime.fromJSDate(this.date_of_death).toISODate();
+    }); 
 
 AuthorSchema
     .virtual('lifeSpan')
     .get(function() {
-      const birthDate = (this.date_of_birth) ? this.date_of_birth.toDateString('en-US', { dateStyle: 'long' }) : '';
-      const deathDate = (this.date_of_death) ? this.date_of_death.toDateString('en-US', { dateStyle: 'long' }) : '';
-      return `${birthDate} - ${deathDate}`;
-    })
-
-AuthorSchema
-    .virtual("deathDateFormatted")
-    .get(function() {
-        //return '2000-10-05';
-        return (this.date_of_death) ? 
-            DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATETIME_MED) : "";
+      const birthDate = (this.date_of_birth) ? this.date_of_birth.toLocaleDateString('en-US', { dateStyle: 'long' }) : '';
+      const deathDate = (this.date_of_death) ? ' - ' + this.date_of_death.toLocaleDateString('en-US', { dateStyle: 'long' }) : '';
+      return `${birthDate}${deathDate}`;
     });
 
 module.exports = mongoose.model("Author", AuthorSchema);
